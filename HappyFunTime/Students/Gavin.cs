e1f0220fa60927be0aa0c2b3e552f3191ac3bda9
gavincolebank <gavincolebank@GAVINCOLEBA38B5>
2021-10-19 00:40:28 -0400

    Class 'Gavin' has been debugged and feature packed

--- HappyFunTime/Students/Gavin.cs
+++ HappyFunTime/Students/Gavin.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/* Gavin Colebank
+ * colebagc@mail.uc.edu
+ * 10/19/21
+ * IT-3045C Contemparary Programming
+ * Assignemnt 08
+ * Due 10/21/2021
+ * -- Fixing errors and implementing play and study methods --
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +16,18 @@ namespace HappyFunTime.Students
 {
     class Gavin : Student
     {
+        /// <summary>
+        /// Method "Play" to print out a message about what Gavin is doing.
+        /// </summary>
+        public override void Play() {
+            Console.WriteLine("Gavin is somewhere getting 8 MPG instead of the advertied 35 +");
+        }
+
+        /// <summary>
+        /// Method "Study" to print out a message about what Gavin is studying.
+        /// </summary>
+        public override void Study() {
+            Console.WriteLine("Gavin is currently studying to figure out how to increase fuel mileage without reducing his heavy foot");
+        }
     }
 }
