58b9f900de35c69c1a65e190267368c2c9301356
abock4 <arbockhorst@gmail.com>
2021-10-19 12:40:47 -0400

    Implemented the abstact members of Student

--- HappyFunTime/Students/Adam.cs
+++ HappyFunTime/Students/Adam.cs
@@ -8,5 +8,19 @@ namespace HappyFunTime.Students
 {
     class Adam : Student
     {
+        /// <summary>
+        /// Implements the inherited member of Student.Study()
+        /// </summary>
+        public override void Study()
+        {
+            Console.WriteLine("Adam is studying how to code in Kotlin.");
+        }
+        /// <summary>
+        /// Implements the inherited member of Student.Play()
+        /// </summary>
+        public override void Play()
+        {
+            Console.WriteLine("Adam is watching The Office on his Xbox instead of playing Modern Warfare");
+        }
     }
 }
