0ff0661b94a2b54313c404053a4186947ee8bcda
buchwasa <buchwasa@outlook.com>
2021-10-20 18:14:23 -0400

    Added Drew class

---
+++ HappyFunTime/Students/Drew.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/**
+ * Drew Buchwald
+ * buchwasa@mail.uc.edu
+ * buchwasa_Assignment08
+ * Course: IT3045 Fall 2021
+ * Due Date: 10/21/2021
+ * Description: Implements the "Drew" child class of Student
+ */
+namespace HappyFunTime.Students
+{
+    class Drew : Student
+    {
+
+        /// <summary>
+        /// Prints out what Drew is currently playing
+        /// </summary>
+        public override void Play()
+        {
+            Console.WriteLine("Drew is playing various games on Xbox");
+        }
+
+        /// <summary>
+        /// Prints out what Drew is currently studing
+        /// </summary>
+        public override void Study()
+        {
+            Console.WriteLine("Drew is studying Rust");
+        }
+    }
+}
