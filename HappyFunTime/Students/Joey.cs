b2d387704ce0f5d89c880e6d2fe44f346361cf5c
dominoPC <iamjoeken@gmail.com>
2021-10-20 22:38:15 -0400

    Implemented Play and Study methods

--- HappyFunTime/Students/Joey.cs
+++ HappyFunTime/Students/Joey.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* Joseph Tankersley tankerjh@mail.uc.edu
+ * Contemporary Programming Fall 2021
+ * Assignment 08
+ * 21 October 2021
+ * Joey class which inherits from Student
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,24 @@ using System.Threading.Tasks;
 
 namespace HappyFunTime.Students
 {
+    /// <summary>
+    /// Joey class implements the Play and Study interfaces inherited from Student
+    /// </summary>
     class Joey: Student
     {
+        /// <summary>
+        /// 'Play' behavior for Joey
+        /// </summary>
+        public override void Play()
+        {
+            Console.WriteLine("Joey is playing guitar");
+        }
+        /// <summary>
+        /// 'Study' behavior for Joey
+        /// </summary>
+        public override void Study()
+        {
+            Console.WriteLine("Joey is studying soldering techniques");
+        }
     }
 }
